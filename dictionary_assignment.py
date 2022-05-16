a8cb30f798b6056648c5c79b12dc057eee39ef89
Kyeong-MyeongGyu <102154478+Kyeong-MyeongGyu@users.noreply.github.com>
2022-05-17 01:52:04 +0900

    python_deepen1

---
+++ dictionary_assignment.py
@@ -0,0 +1,34 @@
+경명규 = {'name' : '경명규', 'age' : '20', 'birthday' : '2003년 6월 4일', 'gender' : '남자', 'phonenumber' : '010-2667-9207'}
+주영빈 = {'name' : '주영빈', 'age' : '22', 'birthday' : '20001년 12월 22일', 'gender' : '여자', 'phonenumber' : '010-3420-5470'}
+김영길 = {'name' : '김영길', 'age' : '20', 'birthday' : '2003년 6월 19일', 'gender' : '남자', 'phonenumber' : '010-2500-8164'}
+name = [1, 2, 3]
+age = [1, 2, 3]
+birthday = [1, 2, 3]
+gender = [1, 2, 3]
+phonenumber = [1, 2, 3]
+
+name[0] = 경명규["name"]
+name[1] = 주영빈["name"]
+name[2] = 김영길["name"]
+print("이름 리스트는 ",name)
+
+age[0] = 경명규["age"]
+age[1] = 주영빈["age"]
+age[2] = 김영길["age"]
+print("나이 리스트는 ",age)
+
+birthday[0] = 경명규["birthday"]
+birthday[1] = 주영빈["birthday"]
+birthday[2] = 김영길["birthday"]
+print("생일 리스트는 ",birthday)
+
+gender[0] = 경명규["gender"]
+gender[1] = 주영빈["gender"]
+gender[2] = 김영길["gender"]
+print("성별 리스트는 ",gender)
+
+phonenumber[0] = 경명규["phonenumber"]
+phonenumber[1] = 주영빈["phonenumber"]
+phonenumber[2] = 김영길["phonenumber"]
+print("전화번호 리스트는 ",phonenumber)
+
---
+++ list_assignment.py
@@ -0,0 +1,20 @@
+alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," "]
+number = ["0","1","2","3","4","5","6","7","8","9","-"]
+
+list1 = alphabet[22]+alphabet[4]+alphabet[26]+alphabet[0]+alphabet[17]+alphabet[4]+alphabet[26]+alphabet[18]+alphabet[10]+alphabet[7]+alphabet[20]+alphabet[26]+alphabet[11]+alphabet[8]+alphabet[14]+alphabet[13]
+print(list1)
+
+list2 = alphabet[7]+alphabet[0]+alphabet[2]+alphabet[10]+alphabet[26]+alphabet[24]+alphabet[14]+alphabet[20]+alphabet[17]+alphabet[26]+alphabet[11]+alphabet[8]+alphabet[5]+alphabet[4]
+print(list2)
+
+list3 = alphabet[12]+alphabet[24]+alphabet[26]+alphabet[13]+alphabet[0]+alphabet[12]+alphabet[4]+alphabet[26]+alphabet[8]+alphabet[18]+alphabet[26]+alphabet[10]+alphabet[12]+alphabet[6]
+print(list3)
+
+list4 = number[2]+number[0]+number[0]+number[3]+number[0]+number[6]+number[0]+number[4]
+print("제 생일은 %s 입니다." %list4)
+
+list5 = number[0]+number[1]+number[0]+number[10]+number[2]+number[6]+number[6]+number[7]+number[10]+number[9]+number[2]+number[0]+number[7]
+print("제 전화번호는 %s 입니다." %list5)
+
+list6 = number[2]+number[0]+number[2]+number[2]+number[1]+number[4]+number[0]+number[8]+number[0]
+print("제 학번은 %s 입니다." %list6)
